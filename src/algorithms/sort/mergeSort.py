ffddeb2947b113f9b8e45eeeb3b4409068aab23d
sourav2406 <sourav2406@gmail.com>
2020-06-08 02:22:08 +0530

    merge sort

---
+++ src/algorithms/sort/mergeSort.py
@@ -0,0 +1,36 @@
+#python3 programe for merge sort
+
+def mergeSort(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        L = arr[:mid]
+        R = arr[mid:]
+
+        mergeSort(L)
+        mergeSort(R)
+
+        i = j = k = 0
+
+        while i < len(L) and j < len(R):
+            if L[i] < R[j]:
+                arr[k] = L[i]
+                i += 1
+            else:
+                arr[k] = R[j]
+                j += 1
+            k += 1
+            
+        while i < len(L):
+            arr[k] = L[i]
+            i += 1
+            k += 1
+        while j < len(R):
+            arr[k] = R[j]
+            j += 1
+            k += 1
+            
+if __name__ == "__main__":
+    arr = [9, 8, 7, 6, 5, 4, 3, 2, 1]
+    print(arr)
+    mergeSort(arr)
+    print(arr)
