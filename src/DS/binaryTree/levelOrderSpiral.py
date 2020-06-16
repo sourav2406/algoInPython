d1b30564a69bf7ade92e10d001f152f2d25f64ff
sourav2406 <sourav2406@gmail.com>
2020-06-16 22:19:14 +0530

    print spiral level order binary tree

---
+++ src/DS/binaryTree/levelOrderSpiral.py
@@ -0,0 +1,48 @@
+#python3 programe for printing level order of binary tree in spiral form
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+def height(node):
+    if node is None:
+        return 0
+    else:
+        lheight = height(node.left)
+        rheight = height(node.right)
+
+        if lheight > rheight:
+            return lheight + 1
+        else:
+            return rheight + 1
+            
+def printGivenLevel(node, level, ltr):
+    if node is None:
+        return
+    if level == 1:
+        print(node.data, end=' ')
+    elif level > 1:
+        if ltr:
+            printGivenLevel(node.left, level - 1, ltr)
+            printGivenLevel(node.right, level - 1, ltr)
+        else:
+            printGivenLevel(node.right, level - 1, ltr)
+            printGivenLevel(node.left, level - 1, ltr)
+            
+def printSpiral(node):
+    h = height(node)
+    ltr = False
+
+    for i in range(1, h + 1):
+        printGivenLevel(node, i, ltr)
+        ltr = not ltr
+
+#driver code
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
+root.left.left = Node(4)
+root.left.right = Node(5)
+
+printSpiral(root)
